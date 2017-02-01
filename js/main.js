81923aa4c68e6914a12adfa087847274614bae2c
Spence Nelson <spenjmn@me.com>
2017-02-01 09:22:20 -0500

    Initial-upload

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$( document ).ready(function() {
+	$( ".circle-outer" ).click(function() {
+	  	$( ".circle" ).removeClass( "active" );
+	  	$( "a" ).removeClass( "active" );
+	  	$( ".circle" ).removeClass( "active-3" );
+	  	$( ".circle-outer" ).addClass( "active" );
+	  	$( ".insight" ).addClass( "active" );
+	});
+ });
+
+$( document ).ready(function() {
+	$( ".circle-middle" ).click(function() {
+	  	$( ".circle" ).removeClass( "active" );
+	  	$( "a" ).removeClass( "active" );
+	  	$( ".circle" ).removeClass( "active-3" );
+	  	$( ".circle-middle" ).addClass( "active" );
+	  	$( ".circle-outer" ).addClass( "active-2" );
+	  	$( ".fact" ).addClass( "active" );
+	});
+ });
+
+$( document ).ready(function() {
+	$( ".circle-inner" ).click(function() {
+	  	$( ".circle" ).removeClass( "active" );
+	  	$( "a" ).removeClass( "active" );
+	  	$( ".circle" ).removeClass( "active-2" );
+	  	$( ".circle-inner" ).addClass( "active" );
+	  	$( ".circle-middle" ).addClass( "active-3" );
+	  	$( ".circle-outer" ).addClass( "active-3" );
+	  	$( ".reverie" ).addClass( "active" );
+
+	});
+ });
